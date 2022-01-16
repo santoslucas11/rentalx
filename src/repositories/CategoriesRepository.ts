6873f8dec2b9fe39e61f478d4ea4958e550f9599
Lucas Rocha <santoslucas.dev@gmail.com>
2022-01-16 03:25:06 -0300

    Primeiro commit, criando primeiras rotas, tipagens e listas de categorias.

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,33 @@
+/* eslint-disable prettier/prettier */
+import { Category } from "../models/Category";
+
+interface ICreateCategoryDTO {
+    name: string,
+    description: string
+}
+
+class CategoriesRepository {
+    private categories: Category[];
+
+    constructor() {
+        this.categories = [];
+    }
+
+    create({ description, name }: ICreateCategoryDTO): void {
+        const category = new Category();
+
+        Object.assign(category, {
+            name,
+            description,
+            create_at: new Date()
+        });
+
+        this.categories.push(category);
+    }
+
+    list(): Category[] {
+        return this.categories;
+    }
+}
+
+export { CategoriesRepository }
---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,25 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable prettier/prettier */
+import { Router } from 'express';
+
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
+
+const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
+
+categoriesRoutes.post("/", (req, res) => {
+    const { name, description } = req.body;
+
+    categoriesRepository.create({ name, description })
+
+    return res.status(201).send();
+})
+
+categoriesRoutes.get("/", (req, res) => {
+    const all = categoriesRepository.list();
+
+    return res.json({ all })
+})
+
+export { categoriesRoutes };
