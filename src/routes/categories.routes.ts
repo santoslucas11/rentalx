bc3bc0114023e2dd6b22e71eff1d5cc4d7409f7a
Lucas Rocha <santoslucas.dev@gmail.com>
2022-01-17 00:45:42 -0300

    Criando services e aplicando o principio SRP de arquitetura S.O.L.I.D.

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -4,18 +4,17 @@
 import { Router } from 'express';
 
 import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { CreateCategoryService } from '../services/CreateCategoryService';
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (req, res) => {
     const { name, description } = req.body;
-    const categoryAlreadyExists = categoriesRepository.findByName(name);
 
-    if (categoryAlreadyExists) {
-        return res.status(400).json({ error: "category already exists" })
-    }
-    categoriesRepository.create({ name, description })
+    const createCategoryService = new CreateCategoryService(categoriesRepository);
+
+    createCategoryService.execute({ name, description });
 
     return res.status(201).send();
 })
